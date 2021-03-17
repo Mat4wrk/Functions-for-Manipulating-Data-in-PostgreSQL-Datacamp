81be4f8578c156d94b79455aab69d6b7856b671d
Mat4wrk <77670765+Mat4wrk@users.noreply.github.com>
2021-03-17 17:10:37 +0700

    Create Adding and subtracting date and time values.sql

---
+++ 2.Working with DATE | TIME Functions and Operators/Adding and subtracting date and time values.sql
@@ -0,0 +1,18 @@
+# Subtract the rental_date from the return_date to calculate the number of days_rented.
+SELECT f.title, f.rental_duration,
+    -- Calculate the number of days rented
+    r.return_date - r.rental_date AS days_rented
+FROM film AS f
+     INNER JOIN inventory AS i ON f.film_id = i.film_id
+     INNER JOIN rental AS r ON i.inventory_id = r.inventory_id
+ORDER BY f.title;
+
+
+# Now use the AGE() function to calculate the days_rented.
+SELECT f.title, f.rental_duration,
+    -- Calculate the number of days rented
+	AGE(r.return_date, r.rental_date) AS days_rented
+FROM film AS f
+	INNER JOIN inventory AS i ON f.film_id = i.film_id
+	INNER JOIN rental AS r ON i.inventory_id = r.inventory_id
+ORDER BY f.title;
