7e8db570d967c5800c33cc652e87bbbcd1806250
Mat4wrk <77670765+Mat4wrk@users.noreply.github.com>
2021-03-17 21:24:34 +0700

    Create The TRIM function.sql

---
+++ 3.Parsing and Manipulating Text/The TRIM function.sql
@@ -0,0 +1,11 @@
+-- Concatenate the uppercase category name and film title
+SELECT 
+  CONCAT(UPPER(c.name), ': ', f.title) AS film_category, 
+  -- Truncate the description remove trailing whitespace
+  TRIM(LEFT(description, 50)) AS film_desc
+FROM 
+  film AS f 
+  INNER JOIN film_category AS fc 
+  	ON f.film_id = fc.film_id 
+  INNER JOIN category AS c 
+  	ON fc.category_id = c.category_id;
