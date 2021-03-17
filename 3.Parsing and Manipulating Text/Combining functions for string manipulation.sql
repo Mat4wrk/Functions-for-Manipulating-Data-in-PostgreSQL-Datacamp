13d04500e27487e636322846b31ae6e6fdb42b5a
Mat4wrk <77670765+Mat4wrk@users.noreply.github.com>
2021-03-17 21:13:45 +0700

    Create Combining functions for string manipulation.sql

---
+++ 3.Parsing and Manipulating Text/Combining functions for string manipulation.sql
@@ -0,0 +1,6 @@
+SELECT
+  -- Extract the characters to the left of the '@'
+  LEFT(email, POSITION('@' IN email)-1) AS username,
+  -- Extract the characters to the right of the '@'
+  SUBSTRING(email FROM POSITION('@' IN email)+1 FOR LENGTH(email)) AS domain
+FROM customer;
