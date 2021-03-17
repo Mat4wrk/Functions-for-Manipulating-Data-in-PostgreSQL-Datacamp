74bff47edeb97040b91bf4f6f9d06b59c4c9a568
Mat4wrk <77670765+Mat4wrk@users.noreply.github.com>
2021-03-17 15:19:57 +0700

    Create Determining data types.sql

---
+++ 1.Overview of Common Data Types/Determining data types.sql
@@ -0,0 +1,8 @@
+-- Get the column name and data type
+SELECT
+ 	column_name, 
+    data_type
+-- From the system database information schema
+FROM INFORMATION_SCHEMA.COLUMNS 
+-- For the customer table
+WHERE table_name = 'customer';
